9db39c904942e55dfc69730d3bd39b40588e7262
Zoe Xanos <zxanos@gmail.com>
2020-05-10 06:51:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,6 +2,9 @@ def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
   new_string = ""
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
   
 end
 
@@ -12,4 +15,17 @@ mixed_data = [
   ["studied", 101, 233, "Ruby"]
 ]
 
-join_nested_strings(mixed_data)
+#join_nested_strings(mixed_data)
+#def find_even_values(src)
+#  row_index = 0
+#  while row_index < src.count do
+#    element_index = 0
+#    while element_index < src[row_index].count do
+#      if src[row_index][element_index].even?
+#        p src[row_index][element_index]
+#      end
+#      element_index += 1
+#    end
+#    row_index += 1
+#  end
+#end
